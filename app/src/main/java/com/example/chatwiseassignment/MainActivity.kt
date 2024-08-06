7ce0f0e4f3dd2d5426260a2ef1d67ca610153aa3
abhizzzeeet <abhijeetbasfore@gmail.com>
2024-08-06 18:18:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/chatwiseassignment/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.chatwiseassignment
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+private lateinit var button: Button
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.view_products_button)
+
+        button.setOnClickListener{
+            val intent = Intent(this, ProductsActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
